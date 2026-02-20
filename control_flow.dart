04d509e48e2621b24018e4ed5573b851a3a86b96
muhammad bunyamin <bunyamin250391@gmail.com>
2026-02-20 15:21:14 +0700

    Implement control flow demonstration with if-else statements

---
+++ control_flow.dart
@@ -0,0 +1,14 @@
+void main() {
+  ifElse();
+}
+
+void ifElse() {
+  int number = 0;
+  if (number > 0) {
+    print("The number is positive.");
+  } else if (number < 0) {
+    print("The number is negative.");
+  } else {
+    print("The number is zero.");
+  }
+}
