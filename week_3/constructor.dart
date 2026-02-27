f4453430d57b4693e2427087ba3988cff4556ce6
muhammad bunyamin <bunyamin250391@gmail.com>
2026-02-27 09:51:45 +0700

    Add Product and Store classes with display methods and main function for demonstration

--- week_3/constructor.dart
+++ week_3/constructor.dart
@@ -1,1 +1,47 @@
+class Product {
+  String name;
+  double price;
 
+  // Constructor
+  Product(this.name, this.price);
+
+  void displayInfo() {
+    print('Product Name: $name');
+    print('Price: \$${price.toStringAsFixed(2)}');
+  }
+}
+
+class Store {
+  String name;
+  String location;
+  List<Product> products;
+
+  // Named constructor
+  Store({required this.name, required this.location, this.products = const []});
+
+  void displayStoreInfo() {
+    print('Store Name: $name');
+    print('Location: $location');
+    print('Products:');
+    for (var product in products) {
+      product.displayInfo();
+    }
+  }
+}
+
+void main() {
+  Product product1 = Product('Laptop', 999.99);
+  Product product2 = Product('Smartphone', 499.99);
+  Product product3 = Product('Headphones', 199.99);
+
+  Store store1 = Store(name: 'Tech Store', location: 'Downtown');
+  Store store2 = Store(
+    location: 'Suburb',
+    name: 'Gadget Hub',
+    products: [product1, product2, product3],
+  );
+
+  store1.displayStoreInfo();
+  print('---');
+  store2.displayStoreInfo();
+}
