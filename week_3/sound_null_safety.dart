ed907ec46c6ad27bda831c56d62e594d974febd1
muhammad bunyamin <bunyamin250391@gmail.com>
2026-02-27 10:24:43 +0700

    Update sound null safety example to use nullable type for 'nama' and add safe access operator

--- week_3/sound_null_safety.dart
+++ week_3/sound_null_safety.dart
@@ -1,10 +1,11 @@
 void main() {
-  String nama = "Ali"; // nama = null; // Error!
+  String? nama = "Ali"; // nama = null; // Error!
 
   int? umur; // umur = null; // No error, umur can be null
   umur = 25;
 
   print("Nama: $nama");
+  print("Panjang nama: ${nama?.length ?? 0}"); // Safe access operator
   print("Umur: $umur");
   print(
     "Umur ali 3 tahun lagi: ${umur != null ? umur + 3 : 'Umur tidak diketahui'}",
